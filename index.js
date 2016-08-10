7f1408ea69c1a1275473f64f3b0684f8729574c3
Ani Pendakur <ani.pendakur@foresee.com>
2016-08-10 13:07:32 -0700

    Added support for custom colors.

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ var webserver = require('./webserver'),
  * @param eventDetails
  */
 var logError = function (eventDetails) {
-  console.log(("[" + strftime('%B %d, %y %H:%M:%S') + "] ").magenta + "Error: ".red + eventDetails.red);
+  console.log(clrs[1]("[" + strftime('%B %d, %y %H:%M:%S') + "] ") + clrs[2]("Error: ") + clrs[2](eventDetails));
 };
 
 /**
  * Log an event
  * @param eventDetails
  */
-var logEvent = function (eventDetails) {
-  console.log(("[" + strftime('%B %d, %y %H:%M:%S') + "] ").magenta + eventDetails.toString().grey);
+var logEvent = function (eventDetails, clrs) {
+  console.log(clrs[1]("[" + strftime('%B %d, %y %H:%M:%S') + "] ") + clrs[4](eventDetails.toString()));
 };
 
 /**
@@ -136,28 +136,38 @@ var isArray = function (arr) {
  */
 function doServerStart(port, dirs, cfg) {
   var lv = logEvent;
-  
+
   if (!cfg.latency) {
     cfg.latency = 0;
   }
+
+  // Colors to be used..
+  clrs = [];
+  clrs[0] = (isArray(cfg.colors) && cfg.colors[0] && colors.hasOwnProperty(cfg.colors[0])) ? colors[cfg.colors[0]] : colors.yellow;
+  clrs[1] = (isArray(cfg.colors) && cfg.colors[1] && colors.hasOwnProperty(cfg.colors[1])) ? colors[cfg.colors[1]] : colors.magenta;
+  clrs[2] = (isArray(cfg.colors) && cfg.colors[2] && colors.hasOwnProperty(cfg.colors[2])) ? colors[cfg.colors[2]] : colors.red;
+  clrs[3] = (isArray(cfg.colors) && cfg.colors[3] && colors.hasOwnProperty(cfg.colors[3])) ? colors[cfg.colors[3]] : colors.blue;
+  clrs[4] = (isArray(cfg.colors) && cfg.colors[4] && colors.hasOwnProperty(cfg.colors[4])) ? colors[cfg.colors[4]] : colors.grey;
+
+
   if (isArray(cfg.latency)) {
     if (cfg.latency.length != 2) {
       cfg.latency = 0;
-      lv("handydevserver error1: latency must be a number or an array of two numbers! Using 0ms latency instead.".red);
+      lv(clrs[2]("handydevserver error1: latency must be a number or an array of two numbers! Using 0ms latency instead."), clrs);
     }
   } else if ((typeof cfg.latency) != 'number') {
-    lv("handydevserver error2: latency must be a number or an array of two numbers! Using 0ms latency instead.".red);
+    lv(clrs[2]("handydevserver error2: latency must be a number or an array of two numbers! Using 0ms latency instead."), clrs);
   }
   var latencymsg = "";
   if (cfg.latency != 0) {
     if (cfg.latency instanceof Array) {
-      latencymsg = " with latency randomized between ".yellow + cfg.latency[0].toString().yellow + 'ms and '.yellow + cfg.latency[1].toString().yellow + 'ms'.yellow;
+      latencymsg = clrs[0](" with latency randomized between ") + clrs[0](cfg.latency[0].toString()) + clrs[0]('ms and ') + clrs[0](cfg.latency[1].toString()) + clrs[0]('ms');
     } else {
-      latencymsg = " with latency of ".yellow + cfg.latency.toString().yellow + 'ms'.yellow;
+      latencymsg = clrs[0](" with latency of ") + clrs[0](cfg.latency.toString()) + clrs[0]('ms');
     }
   }
-  lv("Starting ".yellow + pjson.name.yellow + " " + pjson.version.toString().yellow + (!!cfg.ssl ? ' securely'.magenta : '') + " at ".yellow + ((!!cfg.ssl ? 'https' : 'http') + "://localhost:" + port).blue + latencymsg + "...");
-  lv("Press CTRL-C to stop.".yellow);
+  lv(clrs[0]("Starting ") + " " + clrs[1](pjson.version.toString()) + (!!cfg.ssl ? clrs[1](' securely') : '') + clrs[0](" at ") + clrs[3](((!!cfg.ssl ? 'https' : 'http') + "://localhost:" + port)) + latencymsg + "...", clrs);
+  lv("Press CTRL-C to stop.", clrs);
   if (!isArray(dirs)) {
     dirs = [dirst];
   }
